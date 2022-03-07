a040895138c3caf52db76e19fc62e832253c2871
Daoyuan Wu <daoyuan0x@gmail.com>
2022-03-07 21:03:06 +0800

    add a simple prototype of Ranking

--- rank.js
+++ rank.js
@@ -17,68 +17,29 @@ STELLAR_SERVER = new StellarSdk.Server(SEVER_URL);
 MVT = new StellarSdk.Asset("MVT", "GDPTX2Z3HTJKCHTT5JHCL7M5MD7P2HVV7QUDCTBIHW2BYLO3XR4VSRSA");
 TESTVOTE = new StellarSdk.Asset("TESTVOTE", "GDPTX2Z3HTJKCHTT5JHCL7M5MD7P2HVV7QUDCTBIHW2BYLO3XR4VSRSA");
 
-XLM = new StellarSdk.Asset.native();
-yXLM = new StellarSdk.Asset("yXLM", "GARDNV3Q7YGT4AKSDF25LT32YSCCW4EV22Y2TV3I2PU2MMXJTEDL5T55");
-USDC = new StellarSdk.Asset("USDC", "GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN");
-LSP = new StellarSdk.Asset("LSP", "GAB7STHVD5BDH3EEYXPI3OM7PCS4V443PYB5FNT6CFGJVPDLMKDM24WK");
-AQUA = new StellarSdk.Asset("AQUA", "GBNZILSTVQZ4R7IKQDGHYGY2QXL5QOFJYQMXPKWRRM5PAV7Y4M67AQUA");
-BTC = new StellarSdk.Asset("BTC", "GDPJALI4AZKUU2W426U5WKMAT6CN3AJRPIIRYR2YM54TL2GDWO5O2MZM");
-ETH = new StellarSdk.Asset("ETH", "GBFXOHVAS43OIWNIO7XLRJAHT3BICFEIKOJLZVXNT572MISM4CMGSOCC");
-PYBC = new StellarSdk.Asset("PYBC", "GBVB43NLVIP2USHXSKI7QQCZKZU2Z6U6A5PAHMIW7LLNVMQJTOX2BZI5");
+RANK_CHOICE_1 = StellarSdk.Keypair.fromPublicKey('GDWSVTPOFHEOJBHO7K5MXWIQJKQMQ6VQMBWJQ43CPAUYQ6US5QLS2G7D');
+RANK_CHOICE_2 = StellarSdk.Keypair.fromPublicKey('GD6GYSJZSNXL2IG5VL4XVHWPCETQFWXV7NT4EKO7SBJWWQ6A62SG3QJD');
+RANK_CHOICE_3 = StellarSdk.Keypair.fromPublicKey('GCF7XE4LKRWXVHSLNO5Z3NY3YNWCX6L3YKPKGELH6DW7UZDEHBM3CJ7M');
+RANK_CHOICE_4 = StellarSdk.Keypair.fromPublicKey('GDKGFQ44TD5IWYGFQB4HMNA5KUU62Y5KIT6IOLNWROQ4TWNY55KDZPAR');
+RANK_CHOICE_5 = StellarSdk.Keypair.fromPublicKey('GAUSDEWZGLZJFKZOIN6WJEHJ22JZBNNO5MGOK2MISFEWGYB4V7DC4JPP');
+RANK_CHOICE_6 = StellarSdk.Keypair.fromPublicKey('GCYWV52LUKXQ37G4CG3JILDYCDCXDLNYHACLA6DJ7DUEFAJDFIO65O2P');
 
-XLM_MVT_VOTE = StellarSdk.Keypair.fromPublicKey("GDE5UPEZB63KPP7VVJW3MSVMFT4Z6ATAMDZISWOPRD36D2FOOSSGEHJR");
-MVT_USDC_VOTE = StellarSdk.Keypair.fromPublicKey("GCP3QE6QZLBBCC7I526POQUKEGZUPDKN24J52AGW2IL3BU6IT2OABWGF");
-MVT_yXLM_VOTE = StellarSdk.Keypair.fromPublicKey("GBK2LJBQUZOGRM3SVGHRRY5P7QFSN635HNQZJQSHNJIRS4GQYXJ6LBRG");
-AQUA_MVT_VOTE = StellarSdk.Keypair.fromPublicKey("GCTOZAS33DTMEPQ6K4CCG5LKYV73FKDAZPTJ23Q53MY65BV3LRK7U6TS");
-LSP_MVT_VOTE = StellarSdk.Keypair.fromPublicKey("GD5FVR34DDOGAKB7QM4PBMH6HWWZ6YZJAXO5ZFQ6NQ5OPAUQEO6H3U47");
-MVT_PYBC_VOTE = StellarSdk.Keypair.fromPublicKey("GAGM3GI24FEYNQ6LD62U4LFPXPIOBSQL6QASDXTR6BHJ4EYIX5BDSMCS");
-BTC_MVT_VOTE = StellarSdk.Keypair.fromPublicKey("GCKUUBESSY7SC7G4JEFANHFUKK4XSQ32DOHTQFLAAPPULSMMGZDZZG7V");
-ETH_MVT_VOTE = StellarSdk.Keypair.fromPublicKey("GA4OYMGT7VEHVIXJADAANUFDPXETH5BUHHLMR6TEJPLLZ4EYX457GAGU");
-
-XLM_MVT_POOL = new StellarSdk.LiquidityPoolAsset(XLM, MVT, 30);
-MVT_USDC_POOL = new StellarSdk.LiquidityPoolAsset(MVT, USDC, 30);
-MVT_yXLM_POOL = new StellarSdk.LiquidityPoolAsset(MVT, yXLM, 30);
-AQUA_MVT_POOL = new StellarSdk.LiquidityPoolAsset(AQUA, MVT, 30);
-LSP_MVT_POOL = new StellarSdk.LiquidityPoolAsset(LSP, MVT, 30);
-MVT_PYBC_POOL = new StellarSdk.LiquidityPoolAsset(MVT, PYBC, 30);
-BTC_MVT_POOL = new StellarSdk.LiquidityPoolAsset(BTC, MVT, 30);
-ETH_MVT_POOL = new StellarSdk.LiquidityPoolAsset(ETH, MVT, 30);
-
-XLM_MVT_BURN  = StellarSdk.Keypair.fromPublicKey('GCSJUSJWBSH7NW6HJMFOWQMJXECRVI4JXCCCMYEA5TS6SC625BOFN7CF');
-MVT_USDC_BURN = StellarSdk.Keypair.fromPublicKey('GCLZZAN6SUM4WAZXLSUS65QN23FXMB7IY6TO2FG2SB3MPUL3D3O6KUGG');
-MVT_yXLM_BURN = StellarSdk.Keypair.fromPublicKey('GCGP7YS75XXAD6FIFO6EZR2FDDDDLYPT4PNFGBGVE37ZVVSCDZ3QDPEI');
-AQUA_MVT_BURN = StellarSdk.Keypair.fromPublicKey('GCDWMVJKWC6B3QSPKL2O4YPZYB4ULE34XRLAAQGVQU5GL44ZCVM2PQWP');
-LSP_MVT_BURN  = StellarSdk.Keypair.fromPublicKey('GD7WM5PLNKVM4EZPXKF6I3XEMUMRZJTZVYH5ZJFLP4GDZRJEE66MUDKC');
-MVT_PYBC_BURN = StellarSdk.Keypair.fromPublicKey('GBCFFN3COAQO23YFUFAM26K6H625YEYYJTKUDLC6FATDYXFOIX4K3WOO');
-BTC_MVT_BURN  = StellarSdk.Keypair.fromPublicKey('GBJV575LXINH6WXB4VREFF2KGDFG4Q4P2TZB7AXJRZZURXOYNCWLOJUL');
-ETH_MVT_BURN  = StellarSdk.Keypair.fromPublicKey('GB7OBJWXMM44SG6JBUDIVIONJF3OZ4EKPPL3YJIIUDZHOZNO7QOZTVSF');
 
 CURRENT_LOGIN_METHOD = 0;
 CURRENT_USER_ACCOUNT = "";
 
-PAIRS_LIST = [
-    {'VOTE': XLM_MVT_VOTE, 'POOL': XLM_MVT_POOL, 'BURN': XLM_MVT_BURN},
-    {'VOTE': AQUA_MVT_VOTE, 'POOL': AQUA_MVT_POOL, 'BURN': AQUA_MVT_BURN},
-    {'VOTE': BTC_MVT_VOTE, 'POOL': BTC_MVT_POOL, 'BURN': BTC_MVT_BURN},
-    {'VOTE': ETH_MVT_VOTE, 'POOL': ETH_MVT_POOL, 'BURN': ETH_MVT_BURN},
-    {'VOTE': MVT_yXLM_VOTE, 'POOL': MVT_yXLM_POOL, 'BURN': MVT_yXLM_BURN},
-    {'VOTE': MVT_USDC_VOTE, 'POOL': MVT_USDC_POOL, 'BURN': MVT_USDC_BURN},
-    {'VOTE': LSP_MVT_VOTE, 'POOL': LSP_MVT_POOL, 'BURN': LSP_MVT_BURN},
-    {'VOTE': MVT_PYBC_VOTE, 'POOL': MVT_PYBC_POOL, 'BURN': MVT_PYBC_BURN},
-];
+RANK_LIST = [RANK_CHOICE_1, RANK_CHOICE_2, RANK_CHOICE_3, RANK_CHOICE_4, RANK_CHOICE_5, RANK_CHOICE_6];
 
 NAME_INDEX_DICT = {
-    'XLM_MVOTE': 0,
-    'AQUA_MVOTE': 1,
-    'BTC_MVOTE': 2,
-    'ETH_MVOTE': 3,
-    'MVOTE_yXLM': 4,
-    'MVOTE_USDC': 5,
-    'LSP_MVOTE': 6,
-    'MVOTE_PYBC': 7,
+    'MVT': 0,
+    'AQUA': 1,
+    'YBX': 2,
+    'LSP': 3,
+    'CAFE': 4,
+    'FISH': 5,
 };
 
-PAIR_NUMBER = 8;
+PAIR_NUMBER = 6;
 UPDATE_TIME = 1000*60;  // one min
 UPDATE_SWITCH = 1;
 var mvoteIntervalId;
@@ -174,55 +135,7 @@ function popup_burn(pair) {
         return 0;
     }
     document.getElementById('signupModal_burn').classList.add(pair);
-    let imageNames = pair.split('_');
-    // console.log(imageNames.length, imageNames);
-    for(let i = 0; i < imageNames.length; i++)
-    {   
-        let tmpIdName = 'burn-pair-image-'+(i+1).toString();
-        let tmpPairName = 'pair-ctn-desp-'+(i+1).toString();
-        switch (imageNames[i])
-        {
-            case "XLM":
-                document.getElementById(tmpIdName).src = "https://dynamic-assets.coinbase.com/ddaf9d27a2388105c5568c68ebe4078d057efac1cb9b091af6a57f4d187cf06b2701b95f75bd148d3872df32b69ebb678de71a42da317370aaec7d6448bda379/asset_icons/80782fe2d690f299e7f5bb9b89af87e1db75769e59c14fa0257054c962401805.png";
-                document.getElementById(tmpPairName).innerHTML = "XLM";
-                break;
-            case "MVOTE":
-                document.getElementById(tmpIdName).src = "./icons/MVOTEicon.png";
-                document.getElementById(tmpPairName).innerHTML = "MVT";
-                break;
-            case "USDC":
-                document.getElementById(tmpIdName).src = "https://www.centre.io/images/usdc/usdc-icon-86074d9d49.png";
-                document.getElementById(tmpPairName).innerHTML = "USDC";
-                break;
-            case "yXLM":
-                document.getElementById(tmpIdName).src="https://ultrastellar.com/static/images/icons/yXLM.png";
-                document.getElementById(tmpPairName).innerHTML = "yXLM";
-                break;
-            case "AQUA":
-                document.getElementById(tmpIdName).src="https://aqua.network/assets/img/aqua-logo.png";
-                document.getElementById(tmpPairName).innerHTML = "AQUA";
-                break;
-            case "LSP":
-                document.getElementById(tmpIdName).src="https://lumenswap.io/favicon.ico";
-                document.getElementById(tmpPairName).innerHTML = "LSP";
-                break;
-            case "PYBC":
-                document.getElementById(tmpIdName).src="https://luxpayband.io/img/LXLogo.png";
-                document.getElementById(tmpPairName).innerHTML = "PYBC";
-                break;
-            case "BTC":
-                document.getElementById(tmpIdName).src="https://static.ultrastellar.com/media/assets/img/c3380651-52e5-4054-9121-a438c60a1ec4.png";
-                document.getElementById(tmpPairName).innerHTML = "BTC";
-                break;
-            case "ETH":
-                document.getElementById(tmpIdName).src="https://static.ultrastellar.com/media/assets/img/f50535aa-8fcb-487f-912f-96d338b8e610.png";
-                document.getElementById(tmpPairName).innerHTML = "ETH";
-                break;
-            default:
-                alert('image load wrong');
-                break;
-        }  
-    }
+    
     var blur = document.getElementById('blur');
     blur.classList.toggle('active');
     var popup_bn = document.getElementById('signupModal_burn');
@@ -278,11 +191,7 @@ function logout() {
     document.getElementsByClassName('login_button')[0].innerHTML = 'Connect Wallet';
     CURRENT_LOGIN_METHOD = 0;
     for (let i = 0; i < PAIR_NUMBER; i++) {
-        document.getElementsByClassName('lock_num')[i].innerHTML = "NA";
-        document.getElementsByClassName('lp_num')[i].innerHTML = "NA";
-        document.getElementsByClassName('mvote_num')[i].innerHTML = "NA";
-        document.getElementsByClassName('your_num')[i].innerHTML = "NA";
-        document.getElementsByClassName('burn_num_1')[i].innerHTML = 0;
+        document.getElementsByClassName('rank-your-burn')[i].innerHTML = "0";
     }
     CURRENT_USER_ACCOUNT = "";
     CURRENT_LOGIN_METHOD = 0;
@@ -364,107 +273,13 @@ async function freight_login() {
     check_login();
 }
 
-async function getAquaVote(pairIndex, voteAsset=AQUA, server=STELLAR_SERVER, userAccount=CURRENT_USER_ACCOUNT) {
-    if (userAccount === "") {
-        return 0;
-    }
-
-    var voteKeypair = PAIRS_LIST[pairIndex]['VOTE'];
-
-    var cursor = "";
-    var records = [];
-
-    while (true) {
-        try {
-            var response = await server
-                .claimableBalances()
-                .claimant(voteKeypair.publicKey())
-                .asset(voteAsset)
-                .cursor(cursor)
-                .limit(200)
-                .call();
-        } catch (e) {
-            console.log(`Error ${e} in getAquaVote().`);
-            return 0;
-        }
-
-        var recordList = response['records'];
-
-        if (recordList.length > 0) {
-            records.push.apply(records, recordList);
-            var nextCursor = recordList[recordList.length - 1]['paging_token'];
-            if (cursor !== nextCursor) {
-                cursor = nextCursor;
-                // console.log(`Get cursor: ${cursor}.`);
-            } else {
-                // console.log(`All votes are retrieved.`);
-                break;
-            }
-        } else {
-            // console.log(`All votes are retrieved.`);
-            break;
-        }
-    }
-
-    var userAmount = 0;
-    var totalAmount = 0;
-    for (var recordIndex in records) {
-        var voteAmount = parseFloat(records[recordIndex]['amount']);
-        var claimantAccount = records[recordIndex]['claimants'][1]['destination'];
-        if (claimantAccount === userAccount.accountId()) {
-            userAmount = userAmount + voteAmount;
-        }
-        totalAmount = totalAmount + voteAmount;
-    }
-    console.log(`${records.length} votes in total, ${userAmount}/${totalAmount}`);
-    document.getElementsByClassName('lock_num')[pairIndex].innerHTML = (userAmount * 100/ totalAmount).toFixed(4) + '%';
-    return {'userAmount': userAmount, 'totalAmount': totalAmount};
-}
-
-async function getLPShare(pairIndex, server=STELLAR_SERVER, userAccount=CURRENT_USER_ACCOUNT) {
-    if (userAccount === "") {
-        return 0;
-    }
-
-    var LPAsset = PAIRS_LIST[pairIndex]['POOL'];
-
-    var totalAmount = 0;
-    var userAmount = 0;
-
-    var poolId = LPAsset.toString().split('liquidity_pool:')[1];
-
-    try {
-        var response = await server
-            .liquidityPools()
-            .liquidityPoolId(poolId)
-            .call();
-    } catch (e) {
-        console.log(`Error ${e} in getLPShare().`);
-        return 0;
-    }
-
-    totalAmount = parseFloat(response['total_shares']);
-
-    var userBalances = userAccount['balances'];
-    for (var bIndex in userBalances) {
-        if (userBalances[bIndex]['asset_type'] === "liquidity_pool_shares") {
-            if (userBalances[bIndex]['liquidity_pool_id'] === poolId) {
-                userAmount = parseFloat(userBalances[bIndex]['balance']);
-                break;
-            }
-        }
-    }
-    console.log(`${userAmount}/${totalAmount}`);
-    document.getElementsByClassName('lp_num')[pairIndex].innerHTML = (userAmount * 100/ totalAmount).toFixed(4) + '%';
-    return {'userAmount': userAmount, 'totalAmount': totalAmount};
-}
 
 async function getMVoteBurn(pairIndex, burnAsset=MVT, server=STELLAR_SERVER, userAccount=CURRENT_USER_ACCOUNT) {
     // if (userAccount === "") {
     //     return 0;
     // }
 
-    var burnAccount = PAIRS_LIST[pairIndex]['BURN'];
+    var burnAccount = RANK_LIST[pairIndex];
 
     var cursor = "";
     var records = [];
@@ -526,39 +341,11 @@ async function getMVoteBurn(pairIndex, burnAsset=MVT, server=STELLAR_SERVER, use
         tmpMvoteNum = (tmpMvoteNum).toString();
     }
     if (userAccount !== "") {
-        document.getElementsByClassName('mvote_num')[pairIndex].innerHTML = tmpMvoteNum + '%';
+        document.getElementsByClassName('rank-your-burn')[pairIndex].innerHTML = tmpMvoteNum + '%';
     }
-    document.getElementsByClassName('burn_num_1')[pairIndex].innerHTML = userAmount.toFixed(0);
-    document.getElementsByClassName('burn_num_2')[pairIndex].innerHTML = totalAmount.toFixed(0);   
+    document.getElementsByClassName('rank-your-burn')[pairIndex].innerHTML = userAmount.toFixed(0);
+    document.getElementsByClassName('rank-total-burn')[pairIndex].innerHTML = totalAmount.toFixed(0);   
     return {'userAmount': userAmount, 'totalAmount': totalAmount};
-}
-
-function calReward(pairIndex, voteAmount, lpAmount, burnAmount) {
-    let averageReward, finalReward;
-    averageReward = (voteAmount*1 + lpAmount*2 + burnAmount*1) / 4;
-    finalReward = Math.min(voteAmount, averageReward);
-    document.getElementsByClassName('your_num')[pairIndex].innerHTML = (finalReward*100).toFixed(4) + '%';
-}
-
-function calPairReward(pairIndex, totalBurn, totalVote) {
-    var sumBurn = 0;
-    for (let i = 0; i < totalBurn.length; i++) {
-        sumBurn += totalBurn[i];
-    }
-    var pairReward = totalBurn[pairIndex] / sumBurn * 0.7;
-    var willVoteNumber = 100000;
-    var hourlyReleaseNum = 1000;
-    var maxPairVoteReward;
-    if(totalVote[pairIndex] > 0){
-        if(pairIndex == 0){
-            maxPairVoteReward = hourlyReleaseNum * (0.3+pairReward) * (willVoteNumber / (willVoteNumber+totalVote[pairIndex]));
-        } else {
-            maxPairVoteReward = hourlyReleaseNum * pairReward * (willVoteNumber / (willVoteNumber+totalVote[pairIndex]));
-        }
-        document.getElementsByClassName('estimate_num1')[pairIndex].innerHTML = (maxPairVoteReward*0.25).toFixed(4);
-        document.getElementsByClassName('estimate_num2')[pairIndex].innerHTML = (maxPairVoteReward).toFixed(4);
-    }
-    document.getElementsByClassName('reward_num')[pairIndex].innerHTML = (pairReward*100).toFixed(4) + '%';
 }
 
 function generateTxXDR(pairIndex, burnAmount, burnAsset=MVT, server=STELLAR_SERVER, userAccount=CURRENT_USER_ACCOUNT) {
@@ -566,7 +353,7 @@ function generateTxXDR(pairIndex, burnAmount, burnAsset=MVT, server=STELLAR_SERV
         return 0;
     }
 
-    var burnAccount = PAIRS_LIST[pairIndex]['BURN'];
+    var burnAccount = RANK_LIST[pairIndex];
 
     var tx = new StellarSdk.TransactionBuilder(
         userAccount, {
@@ -667,137 +454,18 @@ function checkTrustline(targetAsset=MVT, server=STELLAR_SERVER, userAccount=CURR
     return assetBalance;
 }
 
-async function getAquaTotalVote(pairIndex, voteAsset=AQUA, server=STELLAR_SERVER) {
-    
-    var voteKeypair = PAIRS_LIST[pairIndex]['VOTE'];
-
-    var cursor = "";
-    var records = [];
-
-    while (true) {
-        try {
-            var response = await server
-                .claimableBalances()
-                .claimant(voteKeypair.publicKey())
-                .asset(voteAsset)
-                .cursor(cursor)
-                .limit(200)
-                .call();
-        } catch (e) {
-            console.log(`Error ${e} in getAquaVote().`);
-            return 0;
-        }
-
-        var recordList = response['records'];
-
-        if (recordList.length > 0) {
-            records.push.apply(records, recordList);
-            var nextCursor = recordList[recordList.length - 1]['paging_token'];
-            if (cursor !== nextCursor) {
-                cursor = nextCursor;
-                // console.log(`Get cursor: ${cursor}.`);
-            } else {
-                // console.log(`All votes are retrieved.`);
-                break;
-            }
-        } else {
-            // console.log(`All votes are retrieved.`);
-            break;
-        }
-    }
-
-    var totalAmount = 0;
-    for (var recordIndex in records) {
-        var voteAmount = parseFloat(records[recordIndex]['amount']);
-        totalAmount = totalAmount + voteAmount;
-    }
-    return {'totalAmount': totalAmount};
-}
-
 async function withoutLogin() {
-    var totalBurn = [];
-    var totalVote = [];
+
     var burnPromise = [];
-    var votePromise = [];
 
-    for (let i = 0; i < PAIR_NUMBER; i++) {
-        burnPromise.push(getMVoteBurn(i));
-        votePromise.push(getAquaTotalVote(i));
-    }
-
-    await Promise.all(burnPromise)
-    .then((MVoteBurn) => {
-        for (let i = 0; i < PAIR_NUMBER; i++) {
-            totalBurn.push(MVoteBurn[i]['totalAmount']);
-        }
-    });
-    await Promise.all(votePromise)
-    .then((AquaVote) => {
-        for (let i = 0; i < PAIR_NUMBER; i++) {
-            totalVote.push(AquaVote[i]['totalAmount']);
-        }
-    });
-    // console.log(`totalVote`, totalVote);
-    // console.log(`totalBurn`, totalBurn);
-
-    for (let i = 0; i < PAIR_NUMBER; i++) {
-        calPairReward(i, totalBurn, totalVote);
-    }
+    for (let i = 0; i < PAIR_NUMBER; i++) {burnPromise.push(getMVoteBurn(i));}
+    await Promise.all(burnPromise);
 }
 
 async function test() {
-    var totalBurn = [];
-    var totalVote = [];
-    var votePromise = [];
-    var lpPromise = [];
+
     var burnPromise = [];
-    var voteAmount = [];
-    var lpAmount = [];
-    var burnAmount = [];
 
-    for (let i = 0; i < PAIR_NUMBER; i++) {
-        votePromise.push(getAquaVote(i));
-        lpPromise.push(getLPShare(i));
-        burnPromise.push(getMVoteBurn(i));
-    }
-
-    await Promise.all(votePromise)
-    .then((AquaVote) => {
-        console.log(AquaVote);
-        for (let i = 0; i < PAIR_NUMBER; i++) {
-            totalVote.push(AquaVote[i]['totalAmount']);
-            if (isNaN(AquaVote[i]['userAmount']/ AquaVote[i]['totalAmount'])){
-                voteAmount.push(0);
-            } else {
-                voteAmount.push(AquaVote[i]['userAmount']/ AquaVote[i]['totalAmount']);
-            }
-        }
-    });
-    await Promise.all(lpPromise)
-    .then((LPShare) => {
-        for (let i = 0; i < PAIR_NUMBER; i++) {
-            if (isNaN(LPShare[i]['userAmount']/ LPShare[i]['totalAmount'])){
-                lpAmount.push(0);
-            } else {
-                lpAmount.push(LPShare[i]['userAmount']/ LPShare[i]['totalAmount']);
-            }
-        }
-    });
-    await Promise.all(burnPromise)
-    .then((MVoteBurn) => {
-        for (let i = 0; i < PAIR_NUMBER; i++) {
-            totalBurn.push(MVoteBurn[i]['totalAmount']);
-            if (isNaN(MVoteBurn[i]['userAmount']/ MVoteBurn[i]['totalAmount'])){
-                burnAmount.push(0);
-            } else {
-                burnAmount.push(MVoteBurn[i]['userAmount']/ MVoteBurn[i]['totalAmount']);
-            }
-        }
-    });
-
-    for (let i = 0; i < PAIR_NUMBER; i++) {
-        calReward(i, voteAmount[i], lpAmount[i], burnAmount[i]);
-        calPairReward(i, totalBurn, totalVote);
-    }
-
+    for (let i = 0; i < PAIR_NUMBER; i++) {burnPromise.push(getMVoteBurn(i));}
+    await Promise.all(burnPromise);
 }
